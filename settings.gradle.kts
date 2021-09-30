8687d20be2b52e55d9bbf97e8d5ebec38d836821
Igor Yankovsky <iyankovsky.tula@gmail.com>
2021-09-30 19:19:25 +0300

    Add a subproject to collect coverage data and generate an aggregated coverage report

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 rootProject.name = "java-server-template"
 
 include(":production:core:entity:common")
+
+include(":testing:code-coverage-report")
---
+++ testing/code-coverage-report/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    `java-library`
+    jacoco
+}
+
+dependencies {
+    implementation(project(":production:core:entity:common"))
+}
+
+// collect source code
+val sourcesPath: Configuration by configurations.creating {
+    extendsFrom(configurations.implementation.get())
+
+    isVisible = false
+    isCanBeResolved = true
+    isCanBeConsumed = false
+
+    attributes {
+        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named("source-directories"))
+        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+    }
+}
+
+// collect coverage data
+val coverageDataPath: Configuration by configurations.creating {
+    extendsFrom(configurations.implementation.get())
+
+    isVisible = false
+    isCanBeResolved = true
+    isCanBeConsumed = false
+
+    attributes {
+        attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named("jacoco-coverage-data"))
+        attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+    }
+}
+
+// gather code coverage from multiple projects
+val codeCoverageReport by tasks.registering(JacocoReport::class) {
+    additionalClassDirs(configurations.runtimeClasspath.get())
+
+    additionalSourceDirs(sourcesPath.incoming.artifactView { lenient(true) }.files)
+    executionData(coverageDataPath.incoming.artifactView { lenient(true) }.files.filter { it.exists() })
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.check {
+    dependsOn(codeCoverageReport)
+}
